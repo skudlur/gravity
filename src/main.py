b88629654ede3fc4606405e38f35cf49d351ce92
suhaskv1 <suhaskv0111@gmail.com>
2022-11-15 02:14:45 -0500

    added ip listing function

--- src/main.py
+++ src/main.py
@@ -4,9 +4,23 @@ from _thread import *
 import os
 
 wiomw_list = who() # who(n)
+ip_list = []
 
-for j in range(0, len(wiomw_list)):
-    comm = f"\n{wiomw_list[j][0]} {wiomw_list[j][1]}\n{wiomw_list[j][2]} {wiomw_list[j][3]}\n{wiomw_list[j][4]} {wiomw_list[j][5]}\n"
-    print(comm)
+def list_connections(wiomw_list):
+    for j in range(0, len(wiomw_list)):
+        comm = f"\n{wiomw_list[j][0]} {wiomw_list[j][1]}\n{wiomw_list[j][2]} {wiomw_list[j][3]}\n{wiomw_list[j][4]} {wiomw_list[j][5]}\n"
+        print(comm)
 
+def ip_connected(wiowm_list, ip_list):
+    for j in range(0, len(wiowm_list)):
+        ip_temp = f"{wiomw_list[j][1]}"
+        if(len(ip_temp) > 15):
+            ip_temp_list = ip_temp.split(", ")
+            ip_temp = ip_temp_list[0]
+            ip_list.append(ip_temp)
+        else:
+            ip_list.append(ip_temp)
+        print(ip_list)
 
+list_connections(wiomw_list)
+ip_connected(wiomw_list, ip_list)
