92b1ee1a75dfcbc4e25bf8951640e54747ce8f2b
suhaskv1 <suhaskv0111@gmail.com>
2022-11-17 02:19:06 -0500

    added ip dict

--- src/main.py
+++ src/main.py
@@ -5,6 +5,8 @@ import os
 
 wiomw_list = who() # who(n)
 ip_list = []
+router_ip = ''
+host_ip = ''
 
 def list_connections(wiomw_list):
     for j in range(0, len(wiomw_list)):
@@ -20,7 +22,29 @@ def ip_connected(wiowm_list, ip_list):
             ip_list.append(ip_temp)
         else:
             ip_list.append(ip_temp)
-        print(ip_list)
+
+def router_ip(wiowm_list, ip_list):
+    stat_ip_list = []
+    for j in range(0, len(wiowm_list)):
+        stat_temp = f'{wiomw_list[j][5]}'
+        stat_ip_list.append(stat_temp)
+    return stat_ip_list
+
+def zip_ip_dict(list1, list2):
+    return dict(zip(list1, list2))
+
+def router_host_ip_set(wiowm_list, ip_list, dict_ip):
+    for j in range(0, len(wiowm_list)):
+        stat_temp = f'{wiomw_list[j][5]}'
+        if("(router)" in stat_temp):
+            router_ip = f'{dict_ip}'
 
 list_connections(wiomw_list)
 ip_connected(wiomw_list, ip_list)
+print(ip_list)
+stat_ip_list = router_ip(wiomw_list, ip_list)
+print(stat_ip_list)
+dict_ip = zip_ip_dict(ip_list, stat_ip_list)
+print(dict_ip)
+
+
